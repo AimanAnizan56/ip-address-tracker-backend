a9645cc0ae30157f348618611bb16fd3a2341bcc
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-12 23:52:45 +0800

    Add index file: contain dummy data for first deploy

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import express, { Express, Request, Response, json } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app: Express = express();
+const port = process.env.PORT || 3030;
+const apiKey = process.env.IPIFY_API_KEY;
+const allowedHost = process.env.ALLOWED_HOST as string;
+const parsedHost = JSON.parse(allowedHost);
+
+const test_data = {
+  ip: '142.250.176.14',
+  location: { country: 'US', region: 'California', city: 'Los Angeles', lat: 34.05223, lng: -118.24368, postalCode: '90001', timezone: '-07:00', geonameId: 5368361 },
+  domains: ['campuslondon.co.uk', 'google.co', 'unternehmen-beispiel.de', 'youtube.com.ua', '1337xxx.xyz'],
+  as: { asn: 15169, name: 'GOOGLE', route: '142.250.176.0/24', domain: 'https://about.google/intl/en/', type: 'Content' },
+  isp: 'Google LLC',
+};
+
+app.use(json());
+app.use(
+  cors({
+    origin: parsedHost.host,
+  })
+);
+
+app.get('/lookup-ip', (req: Request, res: Response) => {
+  const ip = req.headers['cf-connecting-ip'] || req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log('ip', ip);
+  res.json({
+    ...test_data,
+    IP_ADDRESS_TEST: ip,
+  });
+});
+
+app.get('/lookup-ip/ip/:ip_address', (req: Request, res: Response) => {
+  const ip = req.headers['cf-connecting-ip'] || req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log('ip', ip);
+  res.json({
+    ...test_data,
+    IP_ADDRESS_TEST: ip,
+  });
+});
+
+app.get('/lookup-ip/domain/:domain', (req: Request, res: Response) => {
+  const ip = req.headers['cf-connecting-ip'] || req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log('ip', ip);
+  res.json({
+    ...test_data,
+    IP_ADDRESS_TEST: ip,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running`);
+});
