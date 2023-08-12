26e1661061e5be8a6da7de304055b10fa3298ea1
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-13 02:52:53 +0800

    Make request to get ip address

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Express, Request, Response, json } from 'express';
 import ServerlessHttp from 'serverless-http';
 import cors from 'cors';
-import 'dotenv/config';
+import { config } from 'dotenv';
+import { expand } from 'dotenv-expand';
+
+expand(config());
 
 const app: Express = express();
 const port = process.env.PORT || 3030;
-const apiKey = process.env.IPIFY_API_KEY;
+const exactUrl = process.env.EXACT_URL;
 const allowedHost = process.env.ALLOWED_HOST as string;
 const parsedHost = JSON.parse(allowedHost);
 
@@ -27,7 +30,7 @@ app.use(
         const message = 'Someone just trying to access the API from browser!';
         return callback(new Error(message), false);
       }
-      if (allowedHost.indexOf(origin) === -1) {
+      if (parsedHost.host.indexOf(origin) === -1) {
         const message = `Someone just trying to access the API from their website! The origins are ${origin}`;
         return callback(new Error(message), false);
       }
@@ -36,31 +39,31 @@ app.use(
   })
 );
 
-app.get('/lookup-ip', (req: Request, res: Response) => {
-  const ip = req.headers['cf-connecting-ip'] || req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  console.log('ip', ip);
-  res.json({
-    ...test_data,
-    IP_ADDRESS_TEST: ip,
-  });
+app.get('/lookup-ip', async (req: Request, res: Response) => {
+  const ipAddress = req.headers['cf-connecting-ip'] || req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const url = `${exactUrl}&ipAddress=${ipAddress}`;
+  const response = await fetch(url);
+  const data: { code: number } = await response.json();
+  if (data.code) res.status(data.code);
+  res.json(data);
 });
 
-app.get('/lookup-ip/ip/:ip_address', (req: Request, res: Response) => {
-  const ip = req.headers['cf-connecting-ip'] || req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  console.log('ip', ip);
-  res.json({
-    ...test_data,
-    IP_ADDRESS_TEST: ip,
-  });
+app.get('/lookup-ip/ip/:ipAddress', async (req: Request, res: Response) => {
+  const { ipAddress } = req.params;
+  const url = `${exactUrl}&ipAddress=${ipAddress}`;
+  const response = await fetch(url);
+  const data: { code: number } = await response.json();
+  if (data.code) res.status(data.code);
+  res.json(data);
 });
 
-app.get('/lookup-ip/domain/:domain', (req: Request, res: Response) => {
-  const ip = req.headers['cf-connecting-ip'] || req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  console.log('ip', ip);
-  res.json({
-    ...test_data,
-    IP_ADDRESS_TEST: ip,
-  });
+app.get('/lookup-ip/domain/:domain', async (req: Request, res: Response) => {
+  const { domain } = req.params;
+  const url = `${exactUrl}&domain=${domain}`;
+  const response = await fetch(url);
+  const data: { code: number } = await response.json();
+  if (data.code) res.status(data.code);
+  res.json(data);
 });
 
 app.listen(port, () => {
